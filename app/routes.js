57bbde74faa0c2a4ade25d049090f70b7db17f8e
Ralph Hawkins <ralph.hawkins@digital.cabinet-office.gov.uk>
2017-12-28 18:06:08 +0000

    more of the flow

--- app/routes.js
+++ app/routes.js
@@ -12,16 +12,83 @@ module.exports = router
 
 // Branching
 
-router.post('/sole-trader-partnership-details', function (req, res) {
-  // get the answer from the query string (eg. ?over18=false)
-  var typeOfBusiness = req.body.typeOfBusiness
-  console.log(req.body)
+//Are you applying for yourself?
+router.post('/medal-application/active-service', function (req, res) {
 
-  if (typeOfBusiness === 'Limited Company') {
-    // redirect to the relevant page
-    res.redirect('/limited-company-details')
+  var applicationPerson = req.body.applicationPerson
+
+  if (applicationPerson == 'relativeOfDeceased') {
+
+    res.redirect('/medal-application/closest-relative')
   } else {
-    // if over18 is any other value (or is missing) render the page requested
-    res.render('sole-trader-partnership-details')
+
+    res.redirect('/medal-application/active-service')
   }
+})
+
+//Are you retired from the armed forces?
+router.post('/medal-application/which-services', function (req, res) {
+
+  var activeService = req.body.activeService
+
+
+  if (activeService == 'serving') {
+    res.redirect('/medal-application/ineligible-still-serving')  }
+
+  else {
+    res.redirect('/medal-application/which-services')  }
+})
+
+//How were you related to the service person?
+router.post('/medal-application/death-in-service', function (req, res) {
+
+  var relationship = req.body.relationship
+
+  if (relationship == 'relationshipChild') {
+
+    res.redirect('/medal-application/service-person-was-child')
+  } 
+
+  else if (relationship == 'relationshipParent') {
+
+    res.redirect('/medal-application/service-person-was-parent')
+  }
+
+  else if (relationship == 'relationshipSibling') {
+
+    res.redirect('/medal-application/service-person-was-sibling')
+  }
+
+  else if (relationship == 'relationshipGrandParent') {
+
+    res.redirect('/medal-application/service-person-was-grand-parent')
+  }
+
+  else if (relationship == 'relationshipAuntUncle') {
+
+    res.redirect('/medal-application/service-person-was-aunt-uncle')
+  }
+
+  else if (relationship == 'relationshipNieceNephew') {
+
+    res.redirect('/medal-application/service-person-was-niece-nephew')
+  }
+
+  else {
+
+    res.redirect('/medal-application/death-in-service')
+  }
+})
+
+//Did the service person die in service?
+router.post('/medal-application/which-services', function (req, res) {
+
+  var diedInService = req.body.diedInService
+
+
+  if (diedInService == 'diedInServiceFalse') {
+    res.redirect('/medal-application/death-in-service-evidence')  }
+
+  else {
+    res.redirect('/medal-application/which-services')  }
 })
